65109bac2d61f54a33a5cca8ebe85fd84ab81549
kimgura <oli999@naver.com>
2026-03-27 10:12:03 +0900

    Step17 예제 추가함

---
+++ Step17_File2.py
@@ -0,0 +1,24 @@
+# Step17_File2.py
+
+import os
+
+
+if __name__ == "__main__" :
+    
+    # 새로운 파일을 만들어서 문자열을 파일에 출력하기
+
+    letter_path = os.path.join(os.getcwd(), "my_letter.txt")
+
+    # 기존의 내용을 제거하고 덮어쓰기 
+    with open(letter_path, "w", encoding="utf=8") as f:
+        f.write("To my Friend\n")
+        f.write("Hello\n")
+        f.write("Bye\n")
+    
+    # 파일을 열어서 문자열 추가하기 append mode
+    with open(letter_path, "a", encoding="utf-8") as f:
+        f.write("어쩌구.. 저쩌구...\n")
+        f.write("어쩌구.. 저쩌구...\n")
+        f.write("어쩌구.. 저쩌구...\n")
+    
+    print("my_letter.txt 파일 생성 및 쓰기 완료")
